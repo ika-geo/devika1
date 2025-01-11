4d08b0af5db09cdf2ce874819b2f152eaf187f5b
ikascorp <ikagele.scorp@gmail.com>
2025-01-11 18:41:10 +0400

    another change cite

--- src/components/technologiesItem/TechnologiesItem.tsx
+++
@@ -1,9 +0,0 @@
-import "./technologiesItem.css"
-
-const TechnologiesItem = ({technology, color}:{technology:string, color:string}) => {
-    return (
-        <div className="technologiesItem" style={{borderTopColor:`${color}`, color:`${color}`}}>{technology}</div>
-    );
-};
-
-export default TechnologiesItem;
