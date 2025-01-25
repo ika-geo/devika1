a0fbca8583ab9c6e98b2390b7218a5659b55f370
ikascorp <ikagele.scorp@gmail.com>
2025-01-25 22:17:05 +0400

    chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,20 +192,5 @@
         },
     );
 });
-
-
-  /**
-   * chat
-   */
-  <script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
-  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  (function(){
-  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  s1.async=true;
-  s1.src='https://embed.tawk.to/67927389825083258e0a1460/1iia16kkg';
-  s1.charset='UTF-8';
-  s1.setAttribute('crossorigin','*');
-  s0.parentNode.insertBefore(s1,s0);
-  })();
 })();
 
